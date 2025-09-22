c6ac6004ba49bf748c86127b8b3173b705a049aa
Vladimir Iliev <traurigermann@yandex.ru>
2025-09-23 00:02:23 +0300

    added calendar opening logic

--- tests/test_sroll_app.py
+++ tests/test_sroll_app.py
@@ -10,4 +10,7 @@ def test_scroll_to_calendar(driver, report):
         element_names = [name.text for name in elements]
 
         if 'Calendar' in element_names:
+            driver.find_element(AppiumBy.XPATH, "//android.widget.TextView[@text='Calendar']").click()
+            driver.find_element(AppiumBy.ID, "android:id/button1").click()
+            assert driver.find_element(AppiumBy.ID, "com.google.android.calendar:id/header").text == "Google Calendar"
             break
